6defda186eedc3670c648a0cd3007ee821c7bbb3
ZongAng123 <zongang15@yahoo.com>
2019-09-27 19:47:52 +0800

    大波浪

---
+++ TheWaves/WaveView/WaterWaveView.h
@@ -0,0 +1,17 @@
+//
+//  WaterWaveView.h
+//  TheWaves
+//
+//  Created by 纵昂 on 2019/9/27.
+//  Copyright © 2019 纵昂 https://github.com/ZongAng123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WaterWaveView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TheWaves/WaveView/WaterWaveView.m
@@ -0,0 +1,21 @@
+//
+//  WaterWaveView.m
+//  TheWaves
+//
+//  Created by 纵昂 on 2019/9/27.
+//  Copyright © 2019 纵昂 https://github.com/ZongAng123. All rights reserved.
+//
+
+#import "WaterWaveView.h"
+
+@implementation WaterWaveView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
