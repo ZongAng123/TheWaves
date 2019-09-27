12a58bb9ab04a421cdbfd2fa18939e32a0bc99b5
ZongAng123 <zongang15@yahoo.com>
2019-09-27 15:52:57 +0800

    Initial Commit

---
+++ TheWaves/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheWaves
+//
+//  Created by 纵昂 on 2019/9/27.
+//  Copyright © 2019 纵昂 https://github.com/ZongAng123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheWavesTests/TheWavesTests.m
@@ -0,0 +1,37 @@
+//
+//  TheWavesTests.m
+//  TheWavesTests
+//
+//  Created by 纵昂 on 2019/9/27.
+//  Copyright © 2019 纵昂 https://github.com/ZongAng123. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TheWavesTests : XCTestCase
+
+@end
+
+@implementation TheWavesTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
